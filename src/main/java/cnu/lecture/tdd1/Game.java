ab956959c7dd0b6304b2a79b1bb6fe5617cd3916
tchi <tchi@riotgames.com>
2016-04-04 13:07:51 +0900

    Initial setup

---
+++ src/main/java/cnu/lecture/tdd1/Game.java
@@ -0,0 +1,7 @@
+package cnu.lecture.tdd1;
+
+/**
+ * Created by tchi on 2016. 4. 4..
+ */
+public class Game {
+}
---
+++ src/test/java/cnu/lecture/tdd1/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package cnu.lecture.tdd1;
+
+import org.junit.Test;
+
+/**
+ * Created by tchi on 2016. 4. 4..
+ */
+public class BowlingGameTest {
+    @Test
+    public void testGutterGame() {
+        Game g = new Game();
+    }
+}
